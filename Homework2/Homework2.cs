930c6fa6e074e8ab6c4d09261ae06cf61b40b9fa
luka <lukakurkhuli1@gmail.com>
2026-05-21 15:02:05 +0400

    Add Homework2 console app

---
+++ Homework2/Homework2.cs
@@ -0,0 +1,10 @@
+﻿Console.ForegroundColor = ConsoleColor.Blue;
+
+Console.WriteLine("Luka Kashia"); // შენი სახელი და გვარი
+
+Console.Write("შეიყვანე რაიმე: ");
+string input = Console.ReadLine();
+
+Console.WriteLine($"შენ დაწერე: {input}");
+
+Console.ResetColor();
